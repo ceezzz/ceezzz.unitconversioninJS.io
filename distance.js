a73f9e412ea7d69d6c71606cf1959af9e727f4b2
Cesar Jaimes <cesjaimes86@gmail.com>
2024-04-15 12:14:02 -0400

    Update distance.js
    Added first function to convert inches to feet.

--- distance.js
+++ distance.js
@@ -1,1 +1,17 @@
-return 999
+document.addEventListener("DOMContentLoaded", funtion() {
+  const inputDistance = document.getElementById("distance");
+  const btnInToFT = document.getElementById("in_to_ft");
+  const btnFTToIn = document.getElementById("ft_to_in");
+  const distResult = document.getElementById("distresult");
+
+  btnInToFT.addEventListener("click", funtion() {
+    const inches = parseFloat(inputDistance.value);
+    if (!isNAN(inches)) {
+      const feet = inches / 12;
+      distResult = feet;
+    }
+    else {
+      distResult.textContent = "Invalid input!";
+    }
+  });
+})
