39ed45fd395e01ac32dcacbf1d1f70820b8d36f0
Cesar Jaimes <cesjaimes86@gmail.com>
2024-04-15 14:51:57 -0400

    Update temperature.js

--- temperature.js
+++ temperature.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const fahrenheit = parseFloat(inputTemperature.value);
         if (!isNaN(fahrenheit)) {
             const kelvin = (fahrenheit - 32) * 5/9 + 273.15;
-            tempResult.textContent = '${fahrenheit} F = ${kelvin.toFixed(2)} K';
+            tempResult.textContent = `${fahrenheit} &#8451 F = ${kelvin.toFixed(2)} K`;
         } else {
             tempResult.textContent = "Please enter a valid number for fahrenheit.";
         }
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const kelvin = parseFloat(inputTemperature.value);
         if (!isNaN(kelvin)) {
             const fehranheit = (kelvin - 273.15) * 9/5 + 32;
-            tempResult.textContent = '${kelvin} K = ${fehranheit.toFixed(2)} F';
+            tempResult.textContent = `${kelvin} K = ${fehranheit.toFixed(2)} F`;
         } else {
             tempResult.textContent = "Please enter a valid number of kelvin.";
         }
