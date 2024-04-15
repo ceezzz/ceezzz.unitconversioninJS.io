4f868685f8764539b5cf052ff5bd3414fa3c1535
Cesar Jaimes <cesjaimes86@gmail.com>
2024-04-15 14:38:39 -0400

    Update temperature.js

--- temperature.js
+++ temperature.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const inputTemperature = document.getElementById("temperature");
     const btnFToK = document.getElementById("f_to_k");
     const btnKToF = document.getElementById("k_to_f");
-    const tempResult = document.getElementById("distresult");
+    const tempResult = document.getElementById("tempresult");
 
     btnFToK.addEventListener("click", function() {
         const fahrenheit = parseFloat(inputTemperature.value);
